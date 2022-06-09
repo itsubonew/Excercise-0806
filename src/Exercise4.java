f76c1c1a40d6ae9886447bea1ac7b1e68db8aec3
itsubonew <itsubonew@gmail.com>
2022-06-08 17:21:41 -0700

    new excersise done

---
+++ src/Exercise4.java
@@ -0,0 +1,15 @@
+public class Exercise4{
+    public static void main(String[] args) {
+        System.out.printf("%4s%5s%4s%6s%3s","J","a","v","v","a");
+        System.out.printf("%n");
+        System.out.printf("%4s%4s%2s%4s%4s%3s%2s","J","a","a","v","v","a","a");
+        System.out.printf("%n");
+        System.out.printf("%-2s%2s%7s%4s%2s%7s","J","J","aaaaa","V","V","aaaaa");
+        System.out.printf("%n");
+        System.out.printf("%3s%3s%6s%4s%9s","JJ","a","a","V","a","a");
+        System.out.printf("%n");
+    }
+    
+
+}
+
